c59e627e0df39fec9659b8c14dad660ed7676810
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-11-06 15:39:53 +0800

    Fix demo

--- examples/hello-world/serve.js
+++ examples/hello-world/serve.js
@@ -2,5 +2,5 @@ const http = require('http');
 const server = require('./.rigidity/development/server');
 
 http.createServer(server.default).listen(3000).on('listening', () => {
-  console.log('Listening at http://localhost:300')
+  console.log('Listening at http://localhost:3000')
 });
