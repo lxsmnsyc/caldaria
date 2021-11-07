45d37d2766894ffb42cff551389c98fd49c59f78
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-11-07 12:02:13 +0800

    Extend `url` plugin

--- packages/rigidity/env.d.ts
+++ packages/rigidity/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="babel-plugin-solid-labels" />
 
+// queries
 declare module '*?raw' {
   const contents: string;
   export default contents;
@@ -8,4 +9,114 @@ declare module '*?raw' {
 declare module '*?url' {
   const contents: string;
   export default contents;
+}
+
+// images
+
+// images
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+declare module '*.png' {
+  const src: string
+  export default src
+}
+declare module '*.gif' {
+  const src: string
+  export default src
+}
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+declare module '*.ico' {
+  const src: string
+  export default src
+}
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+declare module '*.avif' {
+  const src: string
+  export default src
+}
+
+
+// media
+declare module '*.mp4' {
+  const src: string
+  export default src
+}
+declare module '*.webm' {
+  const src: string
+  export default src
+}
+declare module '*.ogg' {
+  const src: string
+  export default src
+}
+declare module '*.mp3' {
+  const src: string
+  export default src
+}
+declare module '*.wav' {
+  const src: string
+  export default src
+}
+declare module '*.flac' {
+  const src: string
+  export default src
+}
+declare module '*.aac' {
+  const src: string
+  export default src
+}
+
+
+// fonts
+declare module '*.woff' {
+  const src: string
+  export default src
+}
+declare module '*.woff2' {
+  const src: string
+  export default src
+}
+declare module '*.eot' {
+  const src: string
+  export default src
+}
+declare module '*.ttf' {
+  const src: string
+  export default src
+}
+declare module '*.otf' {
+  const src: string
+  export default src
+}
+
+// CSS
+declare module '*.css' {
+  const contents: string;
+  export default contents;
+}
+
+// JSON
+declare module '*.json' {
+  type JSON =
+    | boolean
+    | number
+    | string
+    | null
+    | JSON[]
+    | { [key: string]: JSON };
+
+  const data: JSON;
+  export default data;
 }
--- packages/rigidity/src/plugins/url.ts
+++ packages/rigidity/src/plugins/url.ts
@@ -9,6 +9,23 @@ export default function urlPlugin(): Plugin {
         path: args.path.substring(0, args.path.length - 4),
         namespace: 'url',
       }));
+      // images
+      build.onResolve({ filter: /\.(jpe?g|png|gif|svg|ico|webp|avif)$/ }, (args) => ({
+        path: args.path,
+        namespace: 'url',
+      }));
+      // media
+      build.onResolve({ filter: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/ }, (args) => ({
+        path: args.path,
+        namespace: 'url',
+      }));
+      // font
+      build.onResolve({ filter: /\.(woff2?|eot|ttf|otf)$/ }, (args) => ({
+        path: args.path,
+        namespace: 'url',
+      }));
+
+      // Loader
       build.onLoad({ filter: /.*/, namespace: 'url' }, (args) => ({
         contents: args.path,
         loader: 'file',
