89da44dec709081c3bad908c9c3aa4a5993cc4e1
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-11-16 19:42:47 +0800

    Fix resolution for url

--- packages/rigidity/src/plugins/url.ts
+++ packages/rigidity/src/plugins/url.ts
@@ -1,4 +1,6 @@
 import {
+  OnResolveArgs,
+  OnResolveResult,
   Plugin,
 } from 'esbuild';
 
@@ -9,25 +11,27 @@ export default function urlPlugin(): Plugin {
     async setup(build) {
       const path = await import('path');
       const fs = await import('fs/promises');
+      function normalizeResolve(args: OnResolveArgs): OnResolveResult {
+        if (/^https?:\/\//.test(args.path)) {
+          return {
+            external: true,
+          };
+        }
+        return {
+          path: path.join(args.resolveDir, args.path),
+          namespace: 'url',
+        };
+      }
       build.onResolve({ filter: /\?url$/ }, (args) => ({
         path: path.join(args.resolveDir, args.path.substring(0, args.path.length - 4)),
         namespace: 'url',
       }));
       // images
-      build.onResolve({ filter: /\.(jpe?g|png|gif|svg|ico|webp|avif)$/ }, (args) => ({
-        path: path.join(args.resolveDir, args.path),
-        namespace: 'url',
-      }));
+      build.onResolve({ filter: /\.(jpe?g|png|gif|svg|ico|webp|avif)$/ }, normalizeResolve);
       // media
-      build.onResolve({ filter: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/ }, (args) => ({
-        path: path.join(args.resolveDir, args.path),
-        namespace: 'url',
-      }));
+      build.onResolve({ filter: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/ }, normalizeResolve);
       // font
-      build.onResolve({ filter: /\.(woff2?|eot|ttf|otf)$/ }, (args) => ({
-        path: path.join(args.resolveDir, args.path),
-        namespace: 'url',
-      }));
+      build.onResolve({ filter: /\.(woff2?|eot|ttf|otf)$/ }, normalizeResolve);
 
       // Loader
       build.onLoad({ filter: /.*/, namespace: 'url' }, async (args) => ({
