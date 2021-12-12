e0d5beafe284760bc8bfa9b977c75e3bcae402cc
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-12-12 15:22:48 +0800

    Add vercel demo

---
+++ examples/hello-world/api/serve.js
@@ -0,0 +1,9 @@
+const rigidity = require('rigidity');
+const server = require('./.rigidity/development/server').default;
+
+module.exports = (request, response) => (
+  server(
+    rigidity.createHTTPRequestAdapter(request),
+    rigidity.createHTTPResponseAdapter(response),
+  )
+);
