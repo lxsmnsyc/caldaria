c07401a0a27db26a88f40b731e3a70ef4fc6aabf
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-12-15 22:09:43 +0800

    Update dependencies

--- examples/hello-world/build-src.js
+++ examples/hello-world/build-src.js
@@ -3,7 +3,7 @@ const rigidity = require('rigidity');
 rigidity.createBuild({
   env: process.env.NODE_ENV,
   adapter: process.env.NODE_ENV === 'production' ? 'vercel' : 'http',
-  ssrMode: 'async',
+  ssrMode: 'node-stream',
   esbuild: {
     tsconfig: './tsconfig.json',
   },
