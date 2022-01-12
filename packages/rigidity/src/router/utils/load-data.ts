bc909fda3839818c530292211c9e620b04b03e89
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-13 07:13:13 +0800

    Change data-only flag for data loading

--- packages/rigidity/src/router/utils/load-data.ts
+++ packages/rigidity/src/router/utils/load-data.ts
@@ -1,11 +1,11 @@
-import { RIGIDITY_SEARCH } from '../../constants';
+import { RIGIDITY_DATA } from '../../constants';
 
 export default async function loadData<T>(
   pathname: string,
   search: string,
 ): Promise<T> {
   const params = new URLSearchParams(search);
-  params.set(RIGIDITY_SEARCH, '');
+  params.set(RIGIDITY_DATA, '');
   const response = await fetch(`${pathname}?${params.toString()}`);
   const result = await response.json();
   return result as T;
