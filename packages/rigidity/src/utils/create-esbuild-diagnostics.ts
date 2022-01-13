1979aae4e0f1ce333847d89d7d8097729b5ec83d
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-13 22:14:58 +0800

    Add diagnostics

---
+++ packages/rigidity/src/utils/create-esbuild-diagnostics.ts
@@ -0,0 +1,25 @@
+import { Message } from 'esbuild';
+import { blue, yellow } from './colors';
+import createDiagnosticMessage from './create-diagnostic-message';
+
+function createEsbuildDiagnosticString(message: Message): string {
+  const baseMessage = message.text;
+  if (message.location) {
+    const { location } = message;
+    const file = blue(location.file);
+    const line = yellow(`${location.line}`);
+    const column = yellow(`${location.column + 1}`);
+    return `${file} (${line}, ${column}): ${baseMessage}`;
+  }
+
+  return baseMessage;
+}
+
+export default function generateESBuildDiagnostics(isWarning: boolean, messages: Message[]): void {
+  for (let i = 0, len = messages.length; i < len; i += 1) {
+    createDiagnosticMessage(
+      isWarning ? 'warning' : 'error',
+      createEsbuildDiagnosticString(messages[i]),
+    );
+  }
+}
