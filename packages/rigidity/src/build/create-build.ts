3489fd634195641e7e09ef0673c6649063f8f492
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-13 22:15:13 +0800

    Update create-build.ts

--- packages/rigidity/src/build/create-build.ts
+++ packages/rigidity/src/build/create-build.ts
@@ -1,12 +1,25 @@
 import {
   BuildOptions,
 } from '../types';
+import generateESBuildDiagnostics from '../utils/create-esbuild-diagnostics';
 import createClientBuild from './create-client-build';
 import createServerBuild from './create-server-build';
 
 export default async function createBuild(options: BuildOptions): Promise<void> {
-  await Promise.all([
+  const [server, client] = await Promise.all([
     createServerBuild(options),
     createClientBuild(options),
   ]);
+
+  if (server.errors.length || server.warnings.length) {
+    console.log('Server Build:');
+    generateESBuildDiagnostics(false, server.errors);
+    generateESBuildDiagnostics(true, server.warnings);
+  }
+
+  if (client.errors.length || client.warnings.length) {
+    console.log('Client Build:');
+    generateESBuildDiagnostics(false, client.errors);
+    generateESBuildDiagnostics(true, client.warnings);
+  }
 }
