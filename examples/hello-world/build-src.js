57c19217463b6cdee53f4a3cbbd88007e87e45ae
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-14 10:43:26 +0800

    Update build-src.js

--- examples/hello-world/build-src.js
+++ examples/hello-world/build-src.js
@@ -1,14 +1,14 @@
 const rigidity = require('rigidity');
 const httpAdapter = require('rigidity-adapter-http').default;
-// const vercelAdapter = require('rigidity-adapter-vercel').default;
+const vercelAdapter = require('rigidity-adapter-vercel').default;
 
 rigidity.createBuild({
-  // env: process.env.NODE_ENV,
-  // adapter: process.env.NODE_ENV === 'production' ? 'vercel' : 'http',
-  env: 'development',
-  adapter: httpAdapter,
-  ssrMode: 'node-stream',
-  // ssrMode: 'async',
+  env: process.env.NODE_ENV,
+  adapter: process.env.NODE_ENV === 'production' ? vercelAdapter : httpAdapter,
+  // env: 'development',
+  // adapter: httpAdapter,
+  // ssrMode: 'node-stream',
+  ssrMode: 'async',
   // ssrMode: 'sync',
   esbuild: {
     tsconfig: './tsconfig.json',
