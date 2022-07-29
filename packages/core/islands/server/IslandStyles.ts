3266e0bb66b09844e7aa63693b901c5d036e5649
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-07-29 08:25:23 +0800

    Update IslandStyles.ts

--- packages/core/islands/server/IslandStyles.ts
+++ packages/core/islands/server/IslandStyles.ts
@@ -1,7 +1,7 @@
 import { JSX } from 'solid-js';
 import { escape, ssr, ssrHydrationKey } from 'solid-js/web';
 
-const STYLE = ['<style', ' ', '>', '</style>'];
+const STYLE = ['<style', '>', '</style>'];
 const CONTENT = escape('rigidity-island, rigidity-fragment { display: contents }');
 
 export default function IslandStyles(): JSX.Element {
