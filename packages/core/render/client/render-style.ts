b24681e96a5f4253f328d78e13c54f4a550b8208
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-08-07 14:58:42 +0800

    Fix `renderStyle` compat with hybrid routing

--- packages/core/render/client/render-style.ts
+++ packages/core/render/client/render-style.ts
@@ -1,14 +1,22 @@
 import {
   isServer,
 } from 'solid-js/web';
+import { on } from 'rigidity-hybrid-router';
+
+function insertStyle(id: string, content: string) {
+  if (!document.getElementById(id)) {
+    const el = document.createElement('style');
+    el.id = id;
+    el.textContent = content;
+    document.head.appendChild(el);
+  }
+}
 
 export default function renderStyle(id: string, content: string): void {
   if (!isServer) {
-    if (!document.getElementById(id)) {
-      const el = document.createElement('style');
-      el.id = id;
-      el.textContent = content;
-      document.head.appendChild(el);
-    }
+    on('load', () => {
+      insertStyle(id, content);
+    });
+    insertStyle(id, content);
   }
 }
